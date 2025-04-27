22b0a797f91873e3cbe1bb063ef4823cfcdc6b97
shacharCaduri <shachar.kaduri@gmail.com>
2025-04-27 23:56:14 +0300

    Success fix + new map

--- pokequests/data/event_scripts.s
+++ pokequests/data/event_scripts.s
@@ -1360,3 +1360,6 @@ Text_TestMsg::
 	.include "data/text/save.inc"
 	.include "data/text/new_game_intro.inc"
 	.include "data/text/pokedude.inc"
+
+	.include "data/maps/DreamMap/scripts.inc"
+	.include "data/maps/DreamMap/text.inc"
--- pokequests/include/constants/map_groups.h
+++ pokequests/include/constants/map_groups.h
@@ -272,6 +272,7 @@
 #define MAP_PALLET_TOWN_PLAYERS_HOUSE_2F   (1 | (4 << 8))
 #define MAP_PALLET_TOWN_RIVALS_HOUSE       (2 | (4 << 8))
 #define MAP_PALLET_TOWN_PROFESSOR_OAKS_LAB (3 | (4 << 8))
+#define MAP_DREAM_MAP                      (4 | (4 << 8))
 
 // gMapGroup_IndoorViridian
 #define MAP_VIRIDIAN_CITY_HOUSE             (0 | (5 << 8))
